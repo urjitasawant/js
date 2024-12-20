f11859797ef084a7473a17d69956b91ebcbb7d06
urjitasawant <141426807+urjitasawant@users.noreply.github.com>
2024-12-20 14:04:42 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=144553
+let accountEmail="urjita@google.com"
+var accountPassword="1234"
+accountCity="Mumbai"
+let accountState
+
+//accountId=2
+
+/*
+prefer not to use var 
+because of block scope and function scope
+*/
+
+accountEmail="urji@gmail.com"
+accountPassword="789"
+accountCity="Navi Mumbai"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  //treat all js code as newer version
+
+//alert("hello") we are using node.js not browser
+
+let name="Urjita"
+let age = 22
+let isLoggedIn = false
+
+/*
+number
+bigint
+string
+boolean
+null = standalone value
+undefined (value has not been defined yet)
+symbol =mostly used in react for uniqueness
+
+object
+*/
+
+console.log(typeof "Urjita") // op string
+console.log(typeof null) //op object
+console.log(typeof undefined) //op undefined
